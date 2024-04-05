7751dc2cbd46c540bc3499841b7dbe62701b720b
Arjunmjbhr <arjunmjbhr@gmail.com>
2024-04-05 11:54:24 +0530

    literals.py added

---
+++ literals.py
@@ -0,0 +1,31 @@
+# Literals 
+
+# Literals types:
+
+# 1. Intergers
+#     * octal numbers
+#     * Hexa Decimal Numbers
+# 2. floating point numbers
+# 3. Strings 
+# 4. Booleans
+
+    # numbers 
+
+        # integers 123
+        # octal 0o123
+        # Hexa decimal  0x123
+        # Floating points 123.45
+
+    # Strings
+        # double quotes "Hello!"
+        # single quotes 'Hello!'
+        # Use Quotes within strings 'hi' "Hi"
+        # Escape Quotes 'Hi \'hi\''
+
+    # Boolean
+        # False False
+        # True  True
+        # Numeric False 0
+        # Numeric True 1
+
+print("learn above comments to understand about litereles in python")
