949f4f158b37a7c4d408cf891a195d3601d5c569
Arjunmjbhr <arjunmjbhr@gmail.com>
2024-04-05 12:02:49 +0530

    operators added

---
+++ operators.py
@@ -0,0 +1,22 @@
+# operators 
+
+# Arithamatic Operators 
+
+# + add
+# - Substrct
+# * multiply
+# / divide
+# // floor devide
+# % Modulo
+# ** Exponential
+
+
+# Arithamatic Examples 
+
+print( 2  + 2)
+print(4 - 2)
+print(2 * 3)
+print(10 / 2)
+print(20 // 4)
+print(10 % 3)
+print(3 ** 2)
